702671742ee2719b5fc8aab32ab74e8adb0d0f87
sales <sales@angularadvisors.com>
2020-09-19 03:17:09 -0600

    Initial Source Code Commit

---
+++ OrchardSkills.OrchardCore.OpenIDConnect/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using OrchardSkills.OrchardCore.OpenIDConnect.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrchardSkills.OrchardCore.OpenIDConnect.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
